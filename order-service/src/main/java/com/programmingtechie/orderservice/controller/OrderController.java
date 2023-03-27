f31fea22ea22adef48823e7af57035cf7a00ec13
unnikrishnanr <unnikrishnan.r@innovaturelabs.com>
2023-03-27 09:38:10 +0530

    Inter Process Communication Service Discovery using Netflix Eureka Implement API Gateway using Spring Cloud Gateway Secure Microservices using Keycloak

--- order-service/src/main/java/com/programmingtechie/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/programmingtechie/orderservice/controller/OrderController.java
@@ -2,6 +2,8 @@ package com.programmingtechie.orderservice.controller;
 
 import com.programmingtechie.orderservice.dto.OrderRequest;
 import com.programmingtechie.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -15,8 +17,14 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name="inventory",fallbackMethod = "fallbackMethod")
+    @TimeLimiter(name = "inventory")
     public String placeOrder(@RequestBody OrderRequest orderRequest) {
         orderService.placeOrder(orderRequest);
         return "Order Placed Successfully";
+    }
+
+    public String fallbackMethod(@RequestBody OrderRequest orderRequest,RuntimeException runtimeException) {
+        return "Something Went Wrong, Please order after sometime!";
     }
 }
